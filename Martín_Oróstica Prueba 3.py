4cbeb2ada470d02c241a41da8bb967ad3b0b61be
martinorostica <mar.orostica@duocuc.cl>
2024-06-17 16:29:36 -0400

    Add files via upload

---
+++ Martín_Oróstica Prueba 3.py
@@ -0,0 +1,91 @@
+
+
+while True:
+    print("1.-Agregar Plan")
+    print("2.-Ver Lista de Planes")
+    print("3.-Eliminar plan por ID")
+    print("4.-Generar bbdd")
+    print("5.-Cargar bbdd")
+    print("6.-Ver Estadisticas")
+    print("0.-Salir")
+    
+    op int(input("Seleccione una opcion"))
+    if op ==1:
+       id=int(input("Agregar ID del plan"))
+       nombre=input("Agrege Nombre del propetario que requiere ese Plan")
+       efectividad=int(input("Ingrese el Porcentaje de Efectividad"))
+   
+       
+    def porcentaje de efectividad(x)
+    if valores>=0 and valores <25:
+        categoria="Chiste"
+     
+    elif valores>=26 and valores <50:
+        categoria="Anecdota" 
+     
+    elif valores>=51 and valores <71:
+        categoria="Peligro"
+     
+    elif valores>=76 and valores <99:
+        categoria="Atencion"
+     
+    elif valores>=100:
+        categoria="Esclavitud" 
+                
+    
+    
+    
+    
+    
+    elif op ==2:
+        print(lista)
+        for lis in lista_plan:
+            print(f" Agregar Plan : lis ['Plan']}
+            listar plan : {lis['listar plan']} Eliminar plan por ID : {lis['eliminar plan por ID']} Generar bbdd :{lis['Generar bbdd']} Cargar bbdd:{['Cargar bbdd']} 
+            Estadisticas : {lis['Estadisticas']}")
+                
+        
+        
+    
+    elif op ==3: 
+        encontrado=False
+       print("¿Eliminar tu plan?")
+    plan = input ("ingrese el id de su plan para eliminarlo")
+    for lis int lista_plan:
+    if lis['plan']==plan:
+            print("n\ Plan a eliminar : \n")
+    {list['plan']}   print(f"plan : {lis['plan']}")
+    print("")
+    resp=input("¿Estas seguro que deseas eliminar tu plan?")
+    (si/no)").lower()
+    if(resp=="si")
+        lista_plan.remove(lis)
+    print("Plan Eliminado correctamente")
+    encontrado=True
+    break
+else:
+   print("Plan no eliminado")
+   break
+if encontrado==False:
+ print("Plan no encontrado")  
+ 
+elif op == 4: 
+
+elif op == 5:
+  
+
+    
+ 
+
+
+           
+            
+                
+    
+    
+    
+   
+        
+        
+    
+    
